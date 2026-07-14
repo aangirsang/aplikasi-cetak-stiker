cb6a131616093025b0d51a17991ffa274228a7bc
@Girsang <saiful.girsang@gmail.com>
2026-07-14 12:52:45 +0700

    - pembuatan API PenyesuaianStok

--- src/main/kotlin/com/girsang/stiker/repository/DataRiwayatStokRepository.kt
+++ src/main/kotlin/com/girsang/stiker/repository/DataRiwayatStokRepository.kt
@@ -2,7 +2,6 @@ package com.girsang.stiker.repository
 
 import com.girsang.stiker.model.entity.DataRiwayatStok
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface DataRiwayatStokRepository: JpaRepository<DataRiwayatStok, String> {
     fun findByDataBarangId(barangId: String): List<DataRiwayatStok>
